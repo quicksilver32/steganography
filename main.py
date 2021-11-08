4baf59745e57e105a4dc7cda310b00bd2458785d
quicksilver32 <ameliya9@mail.ru>
2021-11-08 20:33:52 +0300

    GUI

---
+++ main.py
@@ -0,0 +1,101 @@
+import sys
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtGui import QPixmap
+from cv2 import cv2
+import hashlib
+
+import design
+from utils import *
+
+
+def show_message(message):
+    msgBox = QtWidgets.QMessageBox()
+    msgBox.setIcon(QtWidgets.QMessageBox.Information)
+    msgBox.setText(str(message))
+    msgBox.setWindowTitle("Info")
+    msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    msgBox.exec_()
+
+
+class App(QtWidgets.QMainWindow, design.Ui_Steganography):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.decButton.setDisabled(True)
+        self.encButton.setDisabled(True)
+        self.textEdit.setDisabled(True)
+        self.downloadButton.setDisabled(True)
+        self.secretKeyInput.setDisabled(True)
+        self.imgButton.clicked.connect(self.browse_file)
+        self.encButton.clicked.connect(self.encrypt_image)
+        self.decButton.clicked.connect(self.decrypt_image)
+        self.downloadButton.clicked.connect(self.save_file)
+        self.imgFile1 = ""
+        self.imgFile2 = []
+
+    def browse_file(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(self, "Choose file", "", "*.bmp")[0]
+
+        if not file:
+            return
+
+        pixmap = QPixmap(file)
+        self.imgLabel1.setPixmap(pixmap)
+        self.imgLabel2.setPixmap(QPixmap())
+
+        self.imgFile1 = file
+        self.decButton.setDisabled(False)
+        self.encButton.setDisabled(False)
+        self.secretKeyInput.setDisabled(False)
+        self.textEdit.setText("")
+        self.textEdit.setDisabled(False)
+        self.fileLabel.setText(file.split("/")[-1])
+
+    def save_file(self):
+        extension = "." + self.imgFile1.split(".")[-1]
+        file_url = QtWidgets.QFileDialog.getSaveFileName(self, "Save File", "", extension)[0]
+
+        if not file_url:
+            return
+
+        cv2.imwrite(file_url + extension, self.imgFile2)
+
+    def encrypt_image(self):
+        secret_key = self.secretKeyInput.text()
+        text = self.textEdit.toPlainText()
+
+        if secret_key == "" or text == "":
+            show_message("Secret Key and Text fields must be filled!")
+            return
+
+        secret_key = hashlib.md5(secret_key.encode())
+        print(secret_key)
+        img = cv2.imread(self.imgFile1)
+        result = encrypt_image(img, text+"\0", secret_key)
+        image = QtGui.QImage(result.data, result.shape[1], result.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+        self.imgLabel2.setPixmap(QPixmap.fromImage(image))
+        self.imgFile2 = result
+        self.downloadButton.setDisabled(False)
+
+    def decrypt_image(self):
+        secret_key = self.secretKeyInput.text()
+
+        if secret_key == "":
+            show_message("Secret Key field must be filled!")
+            return
+
+        secret_key = hashlib.md5(secret_key.encode())
+        img = cv2.imread(self.imgFile1)
+        result = decrypt_image(img, secret_key)
+        self.textEdit.setText(result)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-from cv2 import cv2
 import random
 
 
@@ -9,70 +8,49 @@ def encrypt_pixel(char, pixel):
     b = (b & 0b11111000) + ((char & 0b11100000) >> 5)
     g = (g & 0b11111100) + ((char & 0b00011000) >> 3)
     r = (r & 0b11111000) + (char & 0b00000111)
-    # print(bin(b), bin(g), bin(r))
     return list((b, g, r))
 
 
 def decrypt_pixel(pixel):
     (b, g, r) = pixel
     char = ((b & 0b00000111) << 5) + ((g & 0b00000011) << 3) + (r & 0b00000111)
-    # print(bin(b), bin(g), bin(r))
     if char > 130:
         char += 890
     return chr(char)
 
 
-def encrypt_image(img):
+def encrypt_image(img, text, seed_key):
     random.seed(seed_key)
     used_pixels = []
     for char in text:
         while True:
-            index = random.randint(0, height * width - 1)
+            index = random.randint(0, img.shape[0] * img.shape[1] - 1)
             if index in used_pixels:
                 continue
             else:
                 used_pixels.append(index)
                 break
-        i, j = index // width, index % height
-        print(index, i,j)
+        i, j = index // img.shape[1], index % img.shape[0]
         img[i][j] = encrypt_pixel(ord(char), img[i][j])
-        # print(i, j, char, bin(ord(char)))
-    cv2.imwrite(input("enc ing: ") + ".bmp", img)
-    return
+    return img
 
 
-def decrypt_image(img):
+def decrypt_image(img, seed_key):
     random.seed(seed_key)
     used_pixels = []
     encrypted_text = ""
     while True:
         while True:
-            index = random.randint(0, height * width - 1)
+            index = random.randint(0, img.shape[0] * img.shape[1] - 1)
             if index in used_pixels:
                 continue
             else:
                 used_pixels.append(index)
                 break
-        i, j = index // width, index % height
+        i, j = index // img.shape[1], index % img.shape[0]
         enc_char = decrypt_pixel(img[i][j])
         if enc_char != "\0":
             encrypted_text += enc_char
         else:
             break
     return encrypted_text
-
-
-seed_key = "123kl21"
-
-if input("encrypt?\n") == "+":
-    start_img = cv2.imread(input("start img: "))
-    text = "НЮХАЙ БЕБРУ!!!! >:}"
-    text += "\0"
-    height, width = start_img.shape[0], start_img.shape[1]
-    print(height, width)
-    encrypt_image(start_img)
-
-if input("decrypt?\n") == "+":
-    enc_img = cv2.imread(input("enc img: "))
-    height, width = enc_img.shape[0], enc_img.shape[1]
-    print(decrypt_image(enc_img))
